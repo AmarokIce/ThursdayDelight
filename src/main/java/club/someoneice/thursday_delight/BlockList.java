c8f38d4e0fc4e2facdc311b7c69eb68547afad7c
HBcao <hbcaoqaq@gmail.com>
2024-03-03 02:25:49 +0800

    1.修复辣椒不能切的问题 2.修复辣椒不掉落的问题 3.修复辣椒没有透明材质的问题 4.修复n个错别字

--- src/main/java/club/someoneice/thursday_delight/BlockList.java
+++ src/main/java/club/someoneice/thursday_delight/BlockList.java
@@ -15,7 +15,7 @@ public class BlockList {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ThursdayMain.MODID);
 
     public static final RegistryObject<Block> CHIZZEA = BLOCKS.register("chizza", () -> new PieBlock(Properties.of(Material.CAKE) , null));
-    public static final RegistryObject<Block> ChilliCrop = BLOCKS.register("chili", ChiliCrop::new);
+    public static final RegistryObject<Block> ChiliCrop = BLOCKS.register("chili", ChiliCrop::new);
     public static final RegistryObject<Block> DinnerPackageBlock = BLOCKS.register("dinner_package", club.someoneice.thursday_delight.dinner_package.DinnerPackageBlock::new);
 
     public static final RegistryObject<Item> DinnerPackage = ItemList.ITEMS.register("dinner_package", () -> new BlockItem(DinnerPackageBlock.get(), new Item.Properties().tab(ThursdayMain.TAB)));
--- src/main/java/club/someoneice/thursday_delight/chili/ChiliCrop.java
+++ src/main/java/club/someoneice/thursday_delight/chili/ChiliCrop.java
@@ -1,25 +1,29 @@
 package club.someoneice.thursday_delight.chili;
 
+import javax.annotation.Nullable;
+
 import club.someoneice.thursday_delight.ItemList;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
-
-import java.util.Random;
+import vectorwing.farmersdelight.common.registry.ModSounds;
 
 public class ChiliCrop extends CropBlock {
+
     public ChiliCrop() {
-        super(BlockBehaviour.Properties.of(Material.PLANT).noCollission());
+        super(BlockBehaviour.Properties.of(Material.PLANT).noCollission().sound(SoundType.CROP));
     }
 
     @Override
@@ -28,12 +32,26 @@ public class ChiliCrop extends CropBlock {
     }
 
     @Override
-    public InteractionResult use(BlockState blockState, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        if (this.isMaxAge(blockState)) {
-            player.getInventory().add(new ItemStack(ItemList.Chili.get(), new Random().nextInt(1, 3)));
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (this.isMaxAge(state)) {
+            // player.getInventory().add(new ItemStack(ItemList.Chili.get(), new Random().nextInt(1, 3)));
+			popResource(level, pos, new ItemStack(ItemList.Chili.get(), level.random.nextInt(1, 3)));
+            level.playSound(null, pos, ModSounds.ITEM_TOMATO_PICK_FROM_BUSH.get(), SoundSource.BLOCKS, 1.0F, 0.8F + level.random.nextFloat() * 0.4F);
+            level.setBlock(pos, state.setValue(this.getAgeProperty(), 0), 2);
             return InteractionResult.SUCCESS;
         } else {
             return InteractionResult.FAIL;
         }
     }
+
+    @Override
+	public void playerDestroy(Level level, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
+		super.playerDestroy(level, player, pos, state, blockEntity, stack);
+        int num = 1;
+		if (this.isMaxAge(state)) {
+			num += level.random.nextInt(1, 3);
+		}
+        popResource(level, pos, new ItemStack(ItemList.Chili.get(), num));
+	}
+
 }
